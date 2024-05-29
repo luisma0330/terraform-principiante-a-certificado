185d7ece140950f14208b714694d496ff8df5aa2
Luis Ojeda <luisma0330@hotmail.com>
2024-05-29 01:10:58 -0600

    19.Constraints

--- practica4/providers.tf
+++ practica4/providers.tf
@@ -2,12 +2,17 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">=4.43.0, <=5.50.0, !=4.43.0"
+      version = ">=4.43.0, <=5.51.1, !=4.43.0"
     }
   }
-  required_version = "~>1.8.0"
+  required_version = "~>1.7.0"
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias = "oregon"
 }
--- practica4/vpc.tf
+++ practica4/vpc.tf
@@ -1,3 +1,16 @@
 resource "aws_vpc" "vpc_virginia" {
-  cidr_block = "10.1.0.0/16"
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "VPC_VIRGINIA"
+    env = "Dev"
+  }
+}
+
+resource "aws_vpc" "vpc_oregon" {
+  cidr_block = "10.20.0.0/16"
+  tags = {
+    Name = "VPC_OREGON"
+    env = "Dev"
+  }
+  provider = aws.oregon
 }
