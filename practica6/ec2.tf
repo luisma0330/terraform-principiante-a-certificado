c45e7fe3024e080236f94fa1620da372aed161f2
Luis Ojeda <luis.manuel.ojeda@accenture.com>
2024-06-05 18:16:26 -0600

    30. Bloques DATA en terraform

--- practica6/ec2.tf
+++ practica6/ec2.tf
@@ -2,4 +2,5 @@ resource "aws_instance" "public_instance" {
   ami = "ami-0d191299f2822b1fa"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public_subnet.id
+  key_name = data.aws_key_pair.key.key_name
 }
