c4af8aabdeee4d5caf451007f4e6aa2819d732b1
Luis Ojeda <luis.manuel.ojeda@accenture.com>
2024-06-13 18:10:37 -0600

    39. Importar recursos a terraform

--- practica6/ec2.tf
+++ practica6/ec2.tf
@@ -42,3 +42,17 @@ resource "aws_instance" "public_instance" {
   #   #   ]
   # }
 }
+
+# resource "aws_instance" "mywebserver" {
+#     ami                                  = "ami-08a0d1e16fc3f61ea"
+#     instance_type                        = "t2.micro"
+#     key_name                             = data.aws_key_pair.key.key_name
+#     subnet_id                            = aws_subnet.public_subnet.id
+#     tags                                 = {
+#         "Name" = "myServer"
+#     }
+#     vpc_security_group_ids               = [
+#         aws_security_group.sg_public_instance.id
+#     ]
+# }
+
